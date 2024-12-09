14f8d2753897d629c4f96b2e00d19dd52d6d14c5
Clodagh Murphy <66846443+clodaghsmurphy@users.noreply.github.com>
2024-12-09 23:15:42 +0100

    monorepo config

--- apps/app-front/vite.config.ts
+++ apps/app-front/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Listen on all interfaces
+    port: 5137, // Ensure the port is specified
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
