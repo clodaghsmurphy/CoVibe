1f892755f019045706d8a30ceb7189a472d6a171
clodagh <clodagh9@yahoo.ie>
2025-05-03 18:23:05 +0100

    sidebar integration

--- apps/app-front/src/App.tsx
+++ apps/app-front/src/App.tsx
@@ -1,25 +1,28 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClientProvider } from "@tanstack/react-query"
 import "./App.css"
-import { SidebarProvider } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppRouter } from "./Router"
-import { AuthProvider } from "@/store/auth-context"
-
-const queryClient = new QueryClient()
-
+import { AuthContext } from "./store/auth-context"
+import { getQueryClient } from "./lib/query-client"
+import { AppSidebar } from "./layout/AppSideBar"
 function App() {
+  const queryClient = getQueryClient()
+
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <AuthProvider>
+        <AuthContext.Provider>
           <div className="min-h-screen bg-background w-full">
             <SidebarProvider>
+              <AppSidebar />
+                <SidebarTrigger />
               <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 <AppRouter />
               </main>
             </SidebarProvider>
           </div>
-        </AuthProvider>
+        </AuthContext.Provider>
       </Router>
     </QueryClientProvider>
   )
