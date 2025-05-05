0cc322b31a7312fed970757b8d645df4db732cd0
clodagh <clodagh9@yahoo.ie>
2025-05-05 12:50:19 +0100

    stly fixes and adding link open a shopping list

--- apps/app-front/src/pages/Groceries/components/GroceriesListCard.tsx
+++ apps/app-front/src/pages/Groceries/components/GroceriesListCard.tsx
@@ -4,20 +4,21 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import dayjs from "dayjs"
 import { TrashIcon } from "lucide-react"
-
+import { useNavigate } from "react-router-dom"
 interface GroceriesListCardProps {
   shoppingList: ShoppingList
 }
 
 export const GroceriesListCard = ({ shoppingList }: GroceriesListCardProps) => {
+  const navigate = useNavigate()
   const { mutate: deleteShoppingList } = useDeleteShoppingList()
   return (
     <Card className=" cursor-pointer transition-colors bg-white border-none">
       <CardContent className="p-4">
-        <div className="flex justify-between items-center">
-          <div className="space-y-1">
+        <div className="flex justify-between items-start">
+          <div className="space-y-1 flex flex-col justify-start text-left">
             <p className="font-medium">{dayjs(shoppingList.createdAt).format("D MMMM YYYY")}</p>
-            <p className="text-sm text-muted-foreground">{shoppingList.items?.length || 0} items</p>
+            <p className="text-sm text-muted">{shoppingList.items?.length || 0} items</p>
           </div>
           <div className="text-right">
             <p className="font-medium">
@@ -29,7 +30,14 @@ export const GroceriesListCard = ({ shoppingList }: GroceriesListCardProps) => {
           </div>
         </div>
         <div className="flex justify-end items-center gap-2">
-          <Button variant="default">View</Button>
+          <Button
+            variant="default"
+            onClick={() => {
+              navigate(`/groceries/shopping-list/${shoppingList.id}`)
+            }}
+          >
+            View
+          </Button>
           <Button variant="ghost" onClick={() => deleteShoppingList(shoppingList.id)}>
             <TrashIcon className="w-4 h-4" />
           </Button>
